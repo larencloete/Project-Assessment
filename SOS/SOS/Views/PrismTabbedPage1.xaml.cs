17679ff4a6ae4eb723497fc46588aedb91a164bc
larencloete <56066363+larencloete@users.noreply.github.com>
2019-12-05 15:34:26 +0200

    My initial commit

---
+++ SOS/SOS/Views/PrismTabbedPage1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SOS.Views
+{
+    public partial class PrismTabbedPage1 : TabbedPage
+    {
+        public PrismTabbedPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
